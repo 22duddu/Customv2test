2e2dcecaee5e5fcbdacaf14437a63b51ce23e028
rohit3607 <rohitplayer87089@gmail.com>
2025-10-16 14:22:34 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -13,7 +13,6 @@ from helper_func import *
 from database.database import *
 
 
-
 # Commands for adding admins by owner
 @Bot.on_message(filters.command('add_admin') & filters.private & filters.user(OWNER_ID))
 async def add_admins(client: Client, message: Message):
@@ -122,19 +121,7 @@ async def get_admins(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]])
     await pro.edit(f"<b>⚡ Current Admin List:</b>\n\n{admin_list}", reply_markup=reply_markup)
 
-import asyncio
-import os
-import random
-import sys
-import time
-from pyrogram import Client, filters, __version__
-from pyrogram.enums import ParseMode, ChatAction, ChatMemberStatus, ChatType
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, ChatMemberUpdated, ChatPermissions
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, InviteHashEmpty, ChatAdminRequired, PeerIdInvalid, UserIsBlocked, InputUserDeactivated, UserNotParticipant
-from bot import Bot
-from config import *
-from helper_func import *
-from database.database import *
+
 
 # Don't Remove Credit @CodeFlix_Bots, @rohit_1888
 # Ask Doubt on telegram @CodeflixSupport
